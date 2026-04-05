20c10e7591c813c1988561db0417ae581013a683
Lucas Granier <lucgcunha@gmail.com>
2026-04-05 15:58:27 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasgcunha/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasgcunha/workshopmongo/services/PostService.java
@@ -6,16 +6,21 @@ import com.lucasgcunha.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PostService {
 
     @Autowired
-    private PostRepository repo;
+    private PostRepository repo;    
 
     public Post findById(String id){
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
