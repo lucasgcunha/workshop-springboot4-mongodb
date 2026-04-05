f9f2f4cebb0f1fd7772e475152e1d3700c557724
Lucas Granier <lucgcunha@gmail.com>
2026-04-05 01:14:48 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lucasgcunha/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lucasgcunha.workshopmongo.repositories;
+
+import com.lucasgcunha.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
