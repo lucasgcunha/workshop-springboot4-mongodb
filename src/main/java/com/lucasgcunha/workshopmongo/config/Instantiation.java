5765dc1e16f3966cdf7b77e035a203677c42c96c
Lucas Granier <lucgcunha@gmail.com>
2026-04-05 01:19:37 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/lucasgcunha/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.lucasgcunha.workshopmongo.config;
+
+import com.lucasgcunha.workshopmongo.domain.User;
+import com.lucasgcunha.workshopmongo.repositories.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String @NonNull ... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    }
+}
